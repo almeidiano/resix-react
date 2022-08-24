31fd8198cae5e3fdf6aa0a84f6e0c00849ee0263
Almeidiano <samuel10ap@gmail.com>
2022-08-24 15:08:21 -0300

    Fixing theme button issue, setting launch button responsive

--- src/components/ToggleButton.js
+++ src/components/ToggleButton.js
@@ -3,14 +3,15 @@ import { faMoon, faSun } from '@fortawesome/free-solid-svg-icons';
 import React, { useState, useRef, useEffect } from 'react';
 
 const ToggleButton = () => {
-    const [toggle, setToggle] = useState(true);
+    const [toggle, setToggle] = useState(localStorage.getItem("theme") === "dark");
     const toggleRef = useRef('');
 
     const ToggleTheme = () => {
-        setToggle(!toggle);
         if(toggle === true) {
+            setToggle(!toggle);
             localStorage.setItem("theme", 'white');
         }else{
+            setToggle(!toggle);
             localStorage.setItem("theme", 'dark');
         }
     }
@@ -19,21 +20,17 @@ const ToggleButton = () => {
 
     useEffect(() => {
         if(theme === 'dark') {
+            console.log('dark');
             toggleRef.current.classList = 'wrg-toggle wrg-toggle--checked';
+            document.documentElement.setAttribute("data-theme", "dark");
+        }else if(theme === 'white') {
+            console.log('white');
+            document.documentElement.setAttribute("data-theme", "white");
+        }else{
+            console.log('white');
+            document.documentElement.setAttribute("data-theme", "white");
         }
     }, [theme]);
-    
-    switch (theme) {
-        case "white":
-            document.documentElement.setAttribute("data-theme", "light");  
-        break;
-        case "dark":
-            document.documentElement.setAttribute("data-theme", "dark");
-        break;
-        case null:
-            document.documentElement.setAttribute("data-theme", "light");
-        break;
-    }
 
     return(
         <div ref={toggleRef} onClick={ToggleTheme} className={`wrg-toggle ${toggle ? 'wrg-toggle--checked' : ''}`}>
